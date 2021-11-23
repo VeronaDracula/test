ca595ee2f79119911ed9748eda00c1de7b67f2ef
Ekaterina Zenkova <e.i.zenkova@yandex.ru>
2021-11-23 19:24:36 +0300

    desktop

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,42 @@
-$(document).ready(function(){
-    $('.header').height($(window).height());
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const carouselText = document.querySelector('.projects-carousel__item-text');
+// const carouselItem = document.querySelector('.owl-item');
+//
+// console.log(carouselItem);
+
+// window.addEventListener('load',function()  {
+//     const carouselItem = document.querySelectorAll('.owl-item');
+//
+//     carouselItem.forEach((item) => {
+//         if(item.classList.contains('active')) {
+//             carouselText.classList.remove('display-none');
+//             console.log(item)
+//         }
+//
+//
+//
+//
+//     })
+
+
+    /*if(!carouselItem.classList.contains('active')) {
+        carouselText.classList.add('display-none');
+        console.log(carouselItem)
+    }
+    else {
+        carouselText.classList.remove('display-none')
+    }*/
+//})
+
---
+++ scripts/jquery-file.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    $('.header').height($(window).height());
+})
+
+//карусель
+$(function() {
+    var owl=$(".projects-carousel");
+    owl.owlCarousel({
+        responsive:{
+            0:{
+                items: 1
+            }
+        },
+        stagePadding: 180,
+        loop: true
+    });
+
+    $(".carousel-arrow_type_right").click(function(){
+        owl.trigger("next.owl.carousel");
+    });
+    $(".carousel-arrow_type_left").click(function(){
+        owl.trigger("prev.owl.carousel");
+    });
+});
+
+//меню
+$(window).scroll(function() {
+    if ($(document).scrollTop() > window.innerHeight) {
+        $(".header__menu-item-link").addClass("header__menu-item-link_scroll");
+        $(".navbar").addClass("navbar_scroll");
+        $(".header__phone").addClass("header__phone_scroll");
+        $(".header__logo").attr('src', './images/logo.svg');
+    } else {
+        $(".header__menu-item-link").removeClass("header__menu-item-link_scroll");
+        $(".navbar").removeClass("navbar_scroll");
+        $(".header__phone").removeClass("header__phone_scroll");
+        $(".header__logo").attr('src', './images/logo-2.svg');
+    }
+});
