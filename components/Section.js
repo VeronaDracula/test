594e118f896e908f565291e047738d85a2fead39
Ekaterina Zenkova <e.i.zenkova@yandex.ru>
2021-11-26 21:41:37 +0300

    not modules

--- components/Section.js
+++ components/Section.js
@@ -1,19 +1,2 @@
 //получение карточки и добавление ее на страницу
 
-export class Section {
-    constructor({items, renderer}, containerSelector) {
-        this._renderedItems = items;
-        this._renderer = renderer;
-        this._container = document.querySelector(containerSelector);
-    }
-
-    addItem(element) {
-        this._container.append(element);
-    }
-
-    renderItems() {
-        this._renderedItems.forEach(item => {
-            this._renderer(item);
-        });
-    }
-}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
-import {Card} from '../components/Card.js'
-
-
-import {Section} from '../components/Section.js'
+//import {Card} from '../components/Card.js'
+//import {Section} from '../components/Section.js'
 
 const initialCards = [
     {
@@ -24,6 +22,69 @@ const initialCards = [
         text: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
     },
 ];
+
+class Section {
+    constructor({items, renderer}, containerSelector) {
+        this._renderedItems = items;
+        this._renderer = renderer;
+        this._container = document.querySelector(containerSelector);
+    }
+
+    addItem(element) {
+        this._container.append(element);
+    }
+
+    renderItems() {
+        this._renderedItems.forEach(item => {
+            this._renderer(item);
+        });
+    }
+}
+
+
+class Card {
+
+    constructor(data, cardSelector, handleCardClick) {
+
+        this._name = data.name;
+        this._text = data.text;
+        this._cardSelector = cardSelector;
+        this._handleCardClick = handleCardClick;
+    }
+
+    _getTemplate() {
+        const cardElement = document
+            .querySelector(this._cardSelector)
+            .content
+            .querySelector('.projects-carousel__item')
+            .cloneNode(true);
+        return cardElement;
+    }
+
+    //создание карточки
+    generateCard() {
+        this._element = this._getTemplate();
+        this._setEventListeners();
+        const cardTitleElement = this._element.querySelector('.projects-carousel__item-title');
+        const cardArticleElement = this._element.querySelector('.projects-carousel__item-article');
+
+        cardTitleElement.textContent = this._name;
+        cardArticleElement.textContent = this._text;
+        return this._element
+    }
+
+    //обработчик для кнопок внутри карточки
+
+    _setEventListeners() {
+
+        const cardLinkElement = this._element.querySelector('.projects-carousel__link');
+
+
+        cardLinkElement.addEventListener('click', (event) => {
+            this._handleCardClick(this._name, this._text);
+        });
+    }
+}
 
 
 
