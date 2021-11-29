61cbe65e0af702da8b03060f48bc366b1d1283e4
Ekaterina Zenkova <e.i.zenkova@yandex.ru>
2021-11-29 10:43:01 +0300

    menu style open

--- pages/index.js
+++ pages/index.js
@@ -139,36 +139,3 @@ api
         initializationCarousel ();
     });
 
-
-
-
-
-// const carouselText = document.querySelector('.projects-carousel__item-text');
-// const carouselItem = document.querySelector('.owl-item');
-//
-// console.log(carouselItem);
-
-// window.addEventListener('load',function()  {
-//     const carouselItem = document.querySelectorAll('.owl-item');
-//
-//     carouselItem.forEach((item) => {
-//         if(item.classList.contains('active')) {
-//             carouselText.classList.remove('display-none');
-//             console.log(item)
-//         }
-//
-//
-//
-//
-//     })
-
-
-    /*if(!carouselItem.classList.contains('active')) {
-        carouselText.classList.add('display-none');
-        console.log(carouselItem)
-    }
-    else {
-        carouselText.classList.remove('display-none')
-    }*/
-//})
-
