acc7bd7b63afeb1546f1fd80f9c553f622530110
Ekaterina Zenkova <e.i.zenkova@yandex.ru>
2022-02-15 15:34:42 +0300

    попап

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
 //выпадающий список
-
 const historyImg = document.querySelector('.history__title-img');
 const history = document.querySelector('.history__title');
+//лайк
+const likeButton = document.querySelector('.offer__like');
+const offerButtonCheck = document.querySelector('.offer__menu-icon');
+//закрытие и открытие попапов
+const popupNotes = document.querySelector('.popup_type_notes');
+const popupNotesOpenButton = document.querySelector('.actions-list__item-link');
+const popupNotesCloseButton = document.querySelector('.form__button_type_cancel');
 
+
+//выпадающий список
 history.addEventListener('click', () => {
     if(historyImg.getAttribute('src') === './images/arr-close.svg') {
         historyImg.setAttribute('src', './images/arr-open.svg');
@@ -15,12 +23,42 @@ history.addEventListener('click', () => {
     }
 });
 
-//лайк
-const likeButton = document.querySelector('.offer__like');
-const offerButtonCheck = document.querySelector('.offer__menu-icon');
 
+//лайк
 likeButton.addEventListener('click', () => {
     likeButton.classList.toggle('offer__like_active');
     offerButtonCheck.classList.toggle('offer__menu-icon_active');
 });
 
+
+//функция закрытия popup
+function closePopup (popup){
+    popup.classList.remove('popup_is-opened');
+    document.removeEventListener('keydown', closeByEsc);
+}
+
+// закрытие popup по Esc
+function closeByEsc (evt) {
+    const popupIsOpened = document.querySelector('.popup_is-opened');
+    if (evt.key === 'Escape') {
+        closePopup(popupIsOpened);
+    }
+}
+
+//закрытие по overlay
+function closePopupByClickOverlay (evt) {
+    const popupIsOpened = document.querySelector('.popup_is-opened');
+    if (evt.target === evt.currentTarget){
+        closePopup(popupIsOpened);
+    }
+}
+
+//функция открытия popup
+function openPopup (popup){
+    popup.classList.add('popup_is-opened');
+    document.addEventListener('keydown', closeByEsc);
+}
+
+popupNotesOpenButton.addEventListener('click', () => openPopup(popupNotes));
+popupNotesCloseButton.addEventListener('click', () => closePopup(popupNotes));
+popupNotes.addEventListener('click', closePopupByClickOverlay);
--- scripts/plugins.js
+++ scripts/plugins.js
@@ -1,15 +1,4 @@
 jQuery(document).ready(function() {
-    //выпадающее меню
-    // jQuery('ul.sf-menu').superfish(
-    //     {
-    //         cssArrows: false,
-    //         delay: 0,
-    //         animation: {opacity:'show'},
-    //         speed: 100,
-    //     }
-    // );
-
-
     //раскрывающийся список
     $(".history").collapse({
         open: function() {
@@ -18,7 +7,5 @@ jQuery(document).ready(function() {
         close: function() {
             this.slideUp(150);
         }
-
     });
-
 });
