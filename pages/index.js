811584e175907890c62f9e1671492359ad9b5856
Ekaterina Zenkova <e.i.zenkova@yandex.ru>
2022-02-15 13:22:58 +0300

    лайк

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+//выпадающий список
+
 const historyImg = document.querySelector('.history__title-img');
 const history = document.querySelector('.history__title');
 
@@ -11,5 +13,14 @@ history.addEventListener('click', () => {
         historyImg.setAttribute('src', './images/arr-close.svg');
         history.classList.remove('history__title_open');
     }
-
 });
+
+//лайк
+const likeButton = document.querySelector('.offer__like');
+const offerButtonCheck = document.querySelector('.offer__menu-icon');
+
+likeButton.addEventListener('click', () => {
+    likeButton.classList.toggle('offer__like_active');
+    offerButtonCheck.classList.toggle('offer__menu-icon_active');
+});
+
